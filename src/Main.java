42c390a581899f72a479e04499527f81d5e33183
Daulet <kantaidaulet@gmail.com>
2024-05-15 18:24:48 +0500

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main {
     public static void main(String[] args) {
         MyHashTable<String,Integer> myHashTable = new MyHashTable<>();
         myHashTable.put("Daulet",19);
-        System.out.println(myHashTable.hashCode());
+        myHashTable.put("Baha",20);
+        myHashTable.put("Lalala",200);
+        System.out.println(myHashTable.get("Lalala"));
+        System.out.println(myHashTable.contains(19));
+        System.out.println(myHashTable.remove("Daulet"));
+        System.out.println(myHashTable.get("Daulet"));
 
     }
 
